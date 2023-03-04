41cb6ce9aa335b9b03a6fbda144ab89b4a62d610
Raquel <80731918+raqueldebug@users.noreply.github.com>
2023-03-04 18:44:44 -0300

    Update ContaEspecial.java

--- ContaEspecial.java
+++ ContaEspecial.java
@@ -8,25 +8,33 @@ public class ContaEspecial extends ContaBancaria {
     // ele coloca o valor e se for uma quantia razoavel (>50,00)o banco empresta
     @Override
     public void sacar() {
-        System.out.println("Caro "+cliente+", qual valor deseja sacar?");
-        float valSacar = in.nextFloat();
-        if (saldo < valSacar) {
+        System.out.println("Caro "+cliente+", qual valor do emprestimo?");
+        float valEmprestimo = in.nextFloat();
 
-            float resultado = saldo - valSacar;
-            System.out.println("Saldo disponível: " + resultado);
+        /* RN= se o valor solicitado for acima de 300 o juros é de 2% ao mês*/
 
 
-            if (resultado < limite) {
-                System.out.println("Saldo disponível para emprestimo: $ 50,00");
-            } else {
-                System.out.println("Acima do limite permitido, não será realizado emprestimo");
-            }
+        /*  double montante = VALOR_TOTAL + VALOR_TOTAL * JUROS;
+		int montanteEmCentavos = (int) (montante * 100);
+		int valorParcela = montanteEmCentavos / NUMERO_PARCELAS;*/
+
+
+        if (valEmprestimo >300 ) {
+
+
+           float mont = (((valEmprestimo+valEmprestimo)*2)/3);
+
+
+
+            System.out.println("Valores acima de 300 reais são cobrados taxa de 2.0 ao mês, " +
+                    "ficando 3 parcelas cada uma no valor de: "+mont);
+
 
         } else {
-            System.out.println("Efetuado com sucesso!");
-            float resultado = saldo - valSacar;
-            System.out.println("Saldo disponível: " + resultado);
 
+            float calsJuros=  valEmprestimo/3;
+            System.out.println("Valores abaixo de 300 reais não são cobrados taxas, portanto " +
+                    "ficará 3 parcelas cada uma no valor de: "+calsJuros);
         }
     }
 
